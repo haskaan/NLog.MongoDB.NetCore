6bfc4d3fa9a0c438e461c9bb14aa1013e1df4277
ByNeo <kaanhas.52@gmail.com>
2017-06-27 16:45:49 +0300

    #1 SonarQube, code quality and analysis tool added.

---
+++ .build/sonar_run.ps1
@@ -0,0 +1,14 @@
+$projectFile = "src\NLog.MongoDB.NetCore\NLog.MongoDB.NetCore.xproj"
+$github = "ByNeo/NLog.MongoDB.NetCore"
+$sonarQubeID = "NLog.MongoDB.NetCore"
+$sonarQubeHost = "https://sonarcloud.io"
+
+SonarQube.Scanner.MSBuild.exe begin \
+                              /k:"$sonarQubeID" \
+                              /d:"sonar.host.url=$sonarQubeHost" \
+                              /d:"sonar.login=$env:sonar_token"
+
+MsBuild.exe $projectFile /t:Rebuild
+
+SonarQube.Scanner.MSBuild.exe end /d:"sonar.login=$env:sonar_token"
+      
