9925c32bbc68884262cc1e017a06e7fa6d50bfe8
ByNeo <kaanhas.52@gmail.com>
2017-06-27 21:50:05 +0300

    #1 sonar_token printed.

--- .build/sonar_run.ps1
+++ .build/sonar_run.ps1
@@ -5,6 +5,8 @@ $sonarQubeHost = "https://sonarcloud.io"
 
 if($env:APPVEYOR_REPO_NAME -eq $github){
 
+	echo $env:sonar_token
+
     if(-not $env:sonar_token){
         Write-warning "not running SonarQube, no sonar_token"
         return;
@@ -19,7 +21,7 @@ if($env:APPVEYOR_REPO_NAME -eq $github){
         MSBuild.SonarQube.Runner.exe begin /k:"$sonarQubeID" /d:"sonar.host.url=$sonarQubeHost" /d:"sonar.organization=byneo-github" /d:"sonar.login=2388c8ee17b65bc2fcc7d45e7ecd6988fe2d5825"
     }
 
-    MsBuild $projectFile /verbosity:minimal
+    MsBuild.exe $projectFile /verbosity:minimal
     MSBuild.SonarQube.Runner.exe end /d:"sonar.login=2388c8ee17b65bc2fcc7d45e7ecd6988fe2d5825"
 
 }else {
