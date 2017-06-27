7b43dcf9c5366f592114008fbf28fdcc40e264ec
ByNeo <kaanhas.52@gmail.com>
2017-06-27 22:10:26 +0300

    #1 sonar_run.ps1 was edited.

--- .build/sonar_run.ps1
+++ .build/sonar_run.ps1
@@ -7,6 +7,8 @@ $sonarToken = "2388c8ee17b65bc2fcc7d45e7ecd6988fe2d5825"
 if($env:APPVEYOR_REPO_NAME -eq $github){
 
 	echo $sonarToken
+	echo $env:APPVEYOR_REPO_NAME
+	echo $env:APPVEYOR_PULL_REQUEST_NUMBER
 
     if(-not $sonarToken){
         Write-warning "not running SonarQube, no sonar_token"
@@ -16,14 +18,14 @@ if($env:APPVEYOR_REPO_NAME -eq $github){
     choco install "msbuild-sonarqube-runner" -y
      
     if ($env:APPVEYOR_PULL_REQUEST_NUMBER) { 
-        MSBuild.SonarQube.Runner.exe begin /k:"$sonarQubeID" /d:"sonar.host.url=$sonarQubeHost" /d:"sonar.organization=byneo-github" /d:"sonar.login=2388c8ee17b65bc2fcc7d45e7ecd6988fe2d5825"
+        MSBuild.SonarQube.Runner.exe begin /k:"$sonarQubeID" /d:"sonar.host.url=$sonarQubeHost" /d:"sonar.organization=byneo-github" /d:"sonar.login=$sonarToken"
     }
     else {
-        MSBuild.SonarQube.Runner.exe begin /k:"$sonarQubeID" /d:"sonar.host.url=$sonarQubeHost" /d:"sonar.organization=byneo-github" /d:"sonar.login=2388c8ee17b65bc2fcc7d45e7ecd6988fe2d5825"
+        MSBuild.SonarQube.Runner.exe begin /k:"$sonarQubeID" /d:"sonar.host.url=$sonarQubeHost" /d:"sonar.organization=byneo-github" /d:"sonar.login=$sonarToken"
     }
 
     MsBuild.exe $projectFile /verbosity:minimal
-    MSBuild.SonarQube.Runner.exe end /d:"sonar.login=2388c8ee17b65bc2fcc7d45e7ecd6988fe2d5825"
+    MSBuild.SonarQube.Runner.exe end /d:"sonar.login=$sonarToken"
 
 }else {
     Write-Output "not running SonarQube as we're on '$env:APPVEYOR_REPO_NAME'"
